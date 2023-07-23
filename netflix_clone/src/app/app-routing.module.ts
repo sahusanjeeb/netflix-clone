b00b2e766b9573a8a4fe9ff4299e53f74d676435
BRD\sanjib.sahu <sanjib.sahu@collabera.com>
2023-07-23 13:28:24 +0530

    create routing

--- netflix_clone/src/app/app-routing.module.ts
+++ netflix_clone/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./pages/landing-page/landing-page.module').then((m) => m.LandingPageModule)
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
